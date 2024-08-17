e97ffe512d12abf0251f16b341818fb9795064e4
Andrea Colleoni <andrea.colleoni@outlook.it>
2024-08-17 16:21:22 +0200

    prima versione del back

---
+++ Debito-Es1/Debito-Es1/Controllers/PiattiController.cs
@@ -0,0 +1,27 @@
+﻿using Debito_Es1.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Debito_Es1.Controllers
+{
+    [Route("api/piatti")]
+    [ApiController]
+    public class PiattiController : ControllerBase
+    {
+        public List<Piatto> ElencoPiatti()
+        {
+            var lista = new List<Piatto>();
+            lista.Add(new Piatto
+            {
+                Nome = "Pasta",
+                Calorie = 150
+            });
+            lista.Add(new Piatto
+            {
+                Nome = "Riso",
+                Calorie = 128
+            });
+            return lista;
+        }
+    }
+}
---
+++ Debito-Es1/Debito-Es1/Model/Piatto.cs
@@ -0,0 +1,8 @@
+﻿namespace Debito_Es1.Model
+{
+    public class Piatto
+    {
+        public string Nome { get; set; }
+        public int Calorie { get; set; }
+    }
+}
